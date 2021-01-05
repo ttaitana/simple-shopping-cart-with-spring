d11d1d505231b1ad90d4865e6447494372493e20
karan sivarat <bomb0069@gmail.com>
2021-01-06 00:51:00 +0700

    [Added]: Test Dummy (alway true) into Application Test

--- src/test/java/com/bomb0069/shopping/ShoppingApplicationTests.java
+++ src/test/java/com/bomb0069/shopping/ShoppingApplicationTests.java
@@ -3,11 +3,14 @@ package com.bomb0069.shopping;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class ShoppingApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
